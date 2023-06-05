415c794fb0f9b6eed70e93732d21a5ea75998db9
xueyinhu <13453894389@163.com>
2023-06-05 23:04:36 +0800

    that

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,34 @@
+export  const HOOKS = [
+    "beforeCreate",
+    "created",
+    "beforeMount",
+    "mounted",
+    "beforeUpdate",
+    "updated",
+    "beforeDestory",
+    "destroyed"
+]
+const strats = {}
+strats.data = function (parentVal, childVal) {
+    return childVal
+}
+
+function mergeHook(parentVal, childVal){
+  if(childVal){
+      if(parentVal){
+           return parentVal.concat(childVal)
+      }else{
+          return [childVal]
+      }
+  }else{
+       return parentVal
+  }
+}
+
+HOOKS.forEach(hooks=>{
+    strats[hooks] = mergeHook
+})
+
 export function mergeOptions(parent, child) {
     const options = {}
     for(let key in parent) {
@@ -8,7 +39,10 @@ export function mergeOptions(parent, child) {
     }
     function mergeField(key) {
         if (start[key]) {
-            options[key] = starts[key](parent[key], child[key])
+            options[key] = strats[key](parent[key], child[key])
+        } else {
+            options[key] = child[key]
         }
     }
+    return options
 }
